112fab50716454d105ff3957541e0f1f3eedc535
unknown <suna@sangon.local>
2017-07-25 15:08:00 +0800

    delete useless files

--- r/point_density.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env R
-# from http://r4ds.had.co.nz/
-
-library(tidyverse)
-data(mpg)
-ggplot(mpg, aes(class, drv)) +  # 离散型x轴和离散型y轴
-    geom_count(aes(color = ..prop.., group = 1)) +  # mdzz
-    scale_size_area(max_size = 20)
-
